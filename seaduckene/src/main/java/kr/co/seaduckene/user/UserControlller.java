d6c3938d30575de90e488bd113f9518d6b1042ff
고진수 <rhwkwk789@gmail.com>
2022-12-28 11:09:42 +0900

    user controller 추가

---
+++ seaduckene/src/main/java/kr/co/seaduckene/user/UserControlller.java
@@ -0,0 +1,31 @@
+package kr.co.seaduckene.user;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserControlller {
+
+	@GetMapping("/userJoin")
+	public void userJoin() {
+
+	}
+
+	@GetMapping("/userMyPage")
+	public void userMyPage() {
+		
+	}
+	
+	@ResponseBody
+	@PostMapping("/checkId")
+	public String checkId(@RequestBody String userId) {
+		System.out.println(userId);
+		return "duplicated";
+	}
+	
+}
