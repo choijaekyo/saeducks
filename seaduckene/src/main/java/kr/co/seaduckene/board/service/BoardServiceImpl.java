cf21a57c51be55e2fde1aec45b7cf226cbea75ed
choijaekyo <chlwory10@naver.com>
2023-01-02 11:06:52 +0900

    board service 수정

--- seaduckene/src/main/java/kr/co/seaduckene/board/service/BoardServiceImpl.java
+++ seaduckene/src/main/java/kr/co/seaduckene/board/service/BoardServiceImpl.java
@@ -1,15 +1,48 @@
 package kr.co.seaduckene.board.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.co.seaduckene.board.command.BoardVO;
 import kr.co.seaduckene.board.mapper.IBoardMapper;
 
 @Service
 public class BoardServiceImpl implements IBoardService {
 
 	@Autowired
-	private IBoardMapper maaper;
+	private IBoardMapper mapper;
+
+	@Override
+	public void regist(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<BoardVO> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardVO content(int bno) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(BoardVO bno) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int bno) {
+		// TODO Auto-generated method stub
+		
+	}
 }
 
 
--- seaduckene/src/main/java/kr/co/seaduckene/board/service/IBoardService.java
+++ seaduckene/src/main/java/kr/co/seaduckene/board/service/IBoardService.java
@@ -1,8 +1,25 @@
 package kr.co.seaduckene.board.service;
 
+import java.util.List;
+
+import kr.co.seaduckene.board.command.BoardVO;
+
 public interface IBoardService {
 
-
+	// 글 등록
+	void regist(BoardVO vo);
+	
+	// 글 목록
+	List<BoardVO> list();
+	
+	// 상세보기
+	BoardVO content(int bno);
+	
+	// 글 수정
+	void update(BoardVO bno);
+	
+	// 글 삭제
+	void delete(int bno);
 }
 
 
