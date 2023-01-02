ca986dfe6af61d7ec4437607c422aa32b5d2ab61
임인규 <allenla@naver.com>
2023-01-02 10:07:57 +0900

    임인규수정본

---
+++ seaduckene/src/main/java/kr/co/seaduckene/board/command/BoardVO.java
@@ -0,0 +1,59 @@
+package kr.co.seaduckene.board.command;
+
+import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+ /*
+CREATE TABLE board(
+    board_no NUMBER PRIMARY KEY,
+    board_title VARCHAR2(60) NOT NULL,
+    board_content VARCHAR2(4000) NOT NULL,
+    board_views  NUMBER DEFAULT 0,
+    board_reg_date DATE DEFAULT sysdate,
+    board_up_date DATE,
+    board_user_no NUMBER NOT NULL,
+    board_category_no NUMBER NOT NULL
+);
+
+CREATE SEQUENCE board_no_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 10000
+    NOCYCLE
+    NOCACHE;
+ */
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor //매개변수 없는 생성자.
+@AllArgsConstructor 
+public class BoardVO {
+   
+   private int boardNo;
+   private String boardTitle;
+   private String boardContent;
+   private int boardViews;
+   private Timestamp boardRegDate;
+   private Timestamp boardUpDate;
+   private int boardUserNo;
+   private int boardCategoryNo;
+   
+}
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
---
+++ seaduckene/src/main/java/kr/co/seaduckene/board/service/IBoardService.java
@@ -0,0 +1,5 @@
+package kr.co.seaduckene.board.service;
+
+public interface IBoardService {
+
+}
